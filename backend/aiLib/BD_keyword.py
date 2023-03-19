c610b5e3312fe59079eb84745f9d727abf6ed06b
returnToInnocence <103014488+returnToInnocence@users.noreply.github.com>
2023-03-19 20:41:35 +0800

    Update BD_keyword.py

--- backend/aiLib/BD_keyword.py
+++ backend/aiLib/BD_keyword.py
@@ -10,8 +10,7 @@ def analyze(text):
     try:
         # 实例化一个认证对象，入参需要传入腾讯云账户 SecretId 和 SecretKey，此处还需注意密钥对的保密
         # 代码泄露可能会导致 SecretId 和 SecretKey 泄露，并威胁账号下所有资源的安全性。以下代码示例仅供参考，建议采用更安全的方式来使用密钥，请参见：https://cloud.tencent.com/document/product/1278/85305
-        # 密钥可前往官网控制台 https://console.cloud.tencent.com/cam/capi 进行获取
-        cred = credential.Credential("AKIDHNGxjJe1H4dz72jFahceVBLHxUt9Bza7", "SgiW6RceZIuLbjtx2x42nLm272pabR6a")
+
         # 实例化一个http选项，可选的，没有特殊需求可以跳过
         httpProfile = HttpProfile()
         httpProfile.endpoint = "nlp.tencentcloudapi.com"
@@ -46,8 +45,6 @@ def analyze(text):
     except TencentCloudSDKException as err:
         print(err)
 
-
-# with open('C:/Users/CCH/Desktop/2.txt', encoding='utf-8') as file:
 #     content = file.read()
 
 # content = "2月23日，据时间视频报道，2月21日，浙江台州一女生考研结束查分，看到成绩后激动到晕倒住院。  " \
